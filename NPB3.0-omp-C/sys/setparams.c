328a6b76f7979c822290a88db3cb30dd3102db32
Qlalq <Qlalq@proton.me>
2025-06-26 17:47:49 +0800

    gcc版本适配

--- NPB3.0-omp-C/sys/setparams.c
+++ NPB3.0-omp-C/sys/setparams.c
@@ -79,7 +79,7 @@ int ilog2(int i);
 
 enum benchmark_types {SP, BT, LU, MG, FT, IS, EP, CG};
 
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int type;
   char class, class_old;
